acd2e032824350853938f2a2d9a254f92ea0445f
Ashish <ashishkumar074880029160@gmail.com>
2025-09-25 15:51:12 +0530

    changes

--- BACKEND/src/config/monogo.config.js
+++ BACKEND/src/config/monogo.config.js
@@ -3,10 +3,7 @@ console.log(process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
